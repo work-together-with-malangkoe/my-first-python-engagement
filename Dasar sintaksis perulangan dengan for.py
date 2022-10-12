448aaaed914f9dfb91f0beccb9afc0a43194cadc
malangkoe <faisalemir99@gmail.com>
2022-10-12 13:01:31 +0700

    perulangan dengan for dan while untuk jumlah buku = 10

---
+++ Dasar sintaksis perulangan dengan for.py
@@ -0,0 +1,13 @@
+"""
+Program perulangan membaca buku dengan FOR
+"""
+jumlah_buku = 10
+print('Ibu berkata, "baca semua bukumu"')
+
+jumlah_buku_yang_sudah_dibaca = 0
+print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
+
+for jumlah_buku_yang_sudah_dibaca in range(1, jumlah_buku+1):
+    print(f"buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+
+print(f"Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
